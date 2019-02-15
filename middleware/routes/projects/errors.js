6f6427b72b014099a736a81fecacf04f685a38ec
joepound <14357797+joepound@users.noreply.github.com>
2019-02-15 10:52:37 -0700

    Completed endpoint for project DELETE request

--- middleware/routes/projects/errors.js
+++ middleware/routes/projects/errors.js
@@ -27,4 +27,33 @@ module.exports = {
     title: "Project POST: failure",
     description: "Could not create project due to an internal server error."
   },
+
+  DELETE_PROJECT_NOT_FOUND: {
+    title: "Project DELETE: project not found",
+    description: "No project with the supplied ID exists",
+    recoveryInstructions: "Please ensure that the correct project ID is supplied."
+  },
+
+  DELETE_PROJECT_EXISTENCE_CHECK_FAILURE: {
+    title: "Project DELETE: could not check project information",
+    description:
+      "Could not check the project database due to an internal server error."
+  },
+
+  DELETE_PROJECT_NO_DELETED_ENTRIES: {
+    title: "Project DELETE: no project entries deleted",
+    description:
+      "The specified project was found in the project database but could not be deleted due to an internal server error."
+  },
+
+  DELETE_PROJECT_MULTIPLE_DELETED_ENTRIES: {
+    title: "Project DELETE: multiple project entries deleted",
+    description:
+      "Multiple project entries were deleted due to an internal server error. We are sorry for the inconvenience."
+  },
+
+  DELETE_PROJECT_FAILURE: {
+    title: "Project DELETE: failure",
+    description: "Could not delete project due to an internal server error."
+  },
 };
--- middleware/routes/projects/routes.js
+++ middleware/routes/projects/routes.js
@@ -54,7 +54,7 @@ router.post("/", (req, res) => {
   console.log("\nAttempting to POST new project...");
 
   const newProject = req.body;
-  
+
   console.log("Checking if required fields were supplied...");
   if (!newProject.name) {
     const code = 400;
@@ -92,6 +92,64 @@ router.post("/", (req, res) => {
       })
       .finally(console.log("Project POST attempt finished."));
   }
+});
+
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+
+  console.log(`\nAttempting to DELETE project ID [${id}]...`);
+
+  console.log("Checking if specified project exists...");
+  projectDB
+    .get(id)
+    .then(project => {
+      if (project) {
+        console.log(`Proceeding to delete project ID [${id}]...`);
+        projectDB
+          .remove(id)
+          .then(deletionCount => {
+            if (deletionCount === 1) {
+              res.status(200).json({
+                success: true,
+                project
+              })
+            } else {
+              const code = 404;
+              res.status(code).json({
+                success: false,
+                code,
+                errorInfo: deletionCount
+                  ? errors.DELETE_PROJECT_MULTIPLE_DELETED_ENTRIES
+                  : errors.DELETE_PROJECT_NO_DELETED_ENTRIES
+              });
+            }
+          })
+          .catch(err => {
+            const code = 500;
+            res.status(code).json({
+              success: false,
+              code,
+              errorInfo: errors.DELETE_PROJECT_FAILURE
+            });
+          });
+      } else {
+        const code = 404;
+        res.status(code).json({
+          success: false,
+          code,
+          errorInfo: errors.DELETE_PROJECT_NOT_FOUND
+        });
+      }
+    })
+    .catch(err => {
+      const code = 500;
+      res.status(code).json({
+        success: false,
+        code,
+        errorInfo: errors.DELETE_PROJECT_EXISTENCE_CHECK_FAILURE
+      });
+    })
+    .finally(console.log(`DELETE attempt for project ID [${id}] finished.`));
 });
 
 module.exports = router;
