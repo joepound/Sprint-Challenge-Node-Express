04876d7cf24e94f4b2c3a40b38df8c2ef7188b18
joepound <14357797+joepound@users.noreply.github.com>
2019-02-15 10:10:19 -0700

    Completed endpoint for project GET request (all)

---
+++ middleware/routes/projects/errors.js
@@ -0,0 +1,7 @@
+module.exports = {
+  GET_ALL_PROJECTS_FAILURE: {
+    title: "Project GET (all): failure",
+    description:
+      "Could not retrieve project database information due to an internal server error."
+  },
+};
--- middleware/routes/projects/routes.js
+++ middleware/routes/projects/routes.js
@@ -4,8 +4,25 @@ const router = express.Router();
 const projectDB = require("./model");
 const actionDB = require("../actions/model");
 
-router.get("/", (req, res) => {
+const errors = require("./errors");
 
+router.get("/", (req, res) => {
+  console.log("\nAttempting to GET all projects...");
+  projectDB
+    .get()
+    .then(projects => res.status(200).json({
+      success: true,
+      projects
+    }))
+    .catch(err => {
+      const code = 500;
+      res.status(code).json({
+        success: false,
+        code,
+        errorInfo: errors.GET_ALL_PROJECTS_FAILURE
+      });
+    })
+    .finally(console.log("GET all projects attempt finished."));
 });
 
 module.exports = router;
