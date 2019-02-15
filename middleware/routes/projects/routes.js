670e72ce22a47bafcaae483ed6bc9788ac53c16d
joepound <14357797+joepound@users.noreply.github.com>
2019-02-15 10:01:52 -0700

    Setup for routes

--- middleware/routes/projects/routes.js
+++ middleware/routes/projects/routes.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
+const projectDB = require("./model");
+const actionDB = require("../actions/model");
 
+router.get("/", (req, res) => {
+
+});
 
 module.exports = router;
