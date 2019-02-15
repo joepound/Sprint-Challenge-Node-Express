b49bd3640aaaaa249d8116f2adb3daec225999fc
joepound <14357797+joepound@users.noreply.github.com>
2019-02-15 09:47:17 -0700

    Setup for middleware

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// play this: https://www.youtube.com/watch?v=d-diB65scQU
+const port = process.env.PORT || 5000;
+
+const server = require("./server");
+server.listen(port, () =>
+  console.log(`Running Node Projects sprint server on port ${port}...`)
+);
